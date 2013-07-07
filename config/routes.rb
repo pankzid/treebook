1a95e2c74b521e947e4192ca04b26ab75d39010a
pankzid <pankzid@gmail.com>
2013-07-08 03:45:44 +0700

    Added test for custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Treebook::Application.routes.draw do
 
   devise_scope :user do
     # get 'register' => 'devise/registrations#new', as: 'register' #or 
-    get 'register', to: 'devise/registrations#new', as: :register
-    get 'logout', to: 'devise/sessions#destroy', as: :logout
-    get 'login', to: 'devise/sessions#new', as: :login
+    get '/register', to: 'devise/registrations#new', as: :register
+    get '/logout', to: 'devise/sessions#destroy', as: :logout
+    get '/login', to: 'devise/sessions#new', as: :login
   end
 
   resources :statuses
 
   root to: 'statuses#index'
-  get 'feed', to: 'statuses#index', as: :feed
+  get '/feed', to: 'statuses#index', as: :feed
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ test/integration/custom_routes_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  
+  test "that /login route should opens login page" do 
+  	get 'login'
+  	assert_response :success
+  end
+
+  test "that /logout route should opens log out page" do 
+  	get 'logout'
+  	assert_response :redirect
+  	assert_redirected_to '/'
+  end
+
+  test "that /register route should opens sign up page" do 
+  	get 'register'
+  	assert_response :success
+  end
+end
