20c350753c7730bf999c2115070f45030f6163e0
pankzid <pankzid@gmail.com>
2013-07-07 13:41:29 +0700

    Writing test validation for user (first_name, last_name, profile_name)

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,4 +4,48 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "a user should enter the first name" do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter the last name" do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter the profile name" do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have unique profile name" do 
+  	user = User.new
+  	user.first_name = 'kinal'
+  	user.last_name = 'devi'
+  	user.email = 'kinal@kinal.com'
+  	user.profile_name = 'kinal'
+
+  	user2 = User.new
+  	user2 = users(:kinal)
+  	user2.save
+
+
+  	assert !user.save
+		assert !user.errors[:profile_name].empty?  	
+  end
+
+  test "a user should have profile name without space" do 
+  	user = User.new
+  	user.profile_name = users(:kinal).profile_name
+
+  	assert !user.save
+		puts user.errors.inspect
+  	assert user.errors[:profile_name].include?("Must be formated correctly.")
+  end
+
 end
